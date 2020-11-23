a2d089dc3c36eb2abf57f5a9f168fd0ab5d189a3
Christopher Roman <chrisjiroman@gmail.com>
2020-11-23 16:19:56 -0500

    footer edit, inline links, font awesome

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,28 +2,28 @@ import React from 'react';
 
 function Footer(props) {
     return (
-        <footer className="site-footer">
+        <footer className="site-footer pt-3">
             <div className="container">
                 <div className="row">             
-                    <div className="col-4 col-sm-2 offset-1">
-                        <h5>Links</h5>
-                        <ul className="list-unstyled">
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">Directory</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Contact</a></li>
+                    <div className="col-4">
+                    <h3>Connect</h3>
+                    <p>chrisjiroman@gmail.com</p>
+                    
+
+                        <a className="btn btn-social-icon btn-instagram" href="http://instagram.com/"><i className="fa fa-envelope" /></a>{' '}
+                        <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/"><i className="fa fa-linkedin" /></a>{' '}
+                        <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-github" /></a>{' '}
+                    </div>
+                    <div className="col-4">
+
+                    </div>
+                    <div className="col-4">
+                    <h5>Navigate</h5>
+                        <ul className="list-inline list-unstyled">
+                            <li className="list-inline-item"><a href="/">project</a></li>
+                            <li className="list-inline-item"><a href="/">resume</a></li>
+                            <li className="list-inline-item"><a href="/">Contact</a></li>
                         </ul>
-                    </div>
-                    <div className="col-6 col-sm-3 text-center">
-                        <h5>Social</h5>
-                        <a className="btn btn-social-icon btn-instagram" href="http://instagram.com/"><i className="fa fa-instagram" /></a>{' '}
-                        <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/"><i className="fa fa-facebook" /></a>{' '}
-                        <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter" /></a>{' '}
-                        <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube" /></a> 
-                    </div>
-                    <div className="col-sm-4 text-center">
-                        <a role="button" className="btn btn-link" href="tel:+12065551234"><i className="fa fa-phone" /> 1-206-555-1234</a><br />
-                        <a role="button" className="btn btn-link" href="mailto:notreal@notreal.co"><i className="fa fa-envelope-o" /> campsites@nucamp.co</a>
                     </div>
                 </div>
             </div>
