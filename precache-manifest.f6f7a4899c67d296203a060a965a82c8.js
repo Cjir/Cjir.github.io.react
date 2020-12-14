1eb0a0534b3c23f771c3fc9cca986a9e7d5cc4e0
Christopher Roman <chrisjiroman@gmail.com>
2020-12-14 17:02:20 -0500

    Updates

---
+++ precache-manifest.f6f7a4899c67d296203a060a965a82c8.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "680c6a2e68da7fe4e9dddb38bbda9722",
+    "url": "/chris.r.io/index.html"
+  },
+  {
+    "revision": "c87b7be019e482e6f1cc",
+    "url": "/chris.r.io/static/css/2.b0752325.chunk.css"
+  },
+  {
+    "revision": "bbf2c7f1e228d0384147",
+    "url": "/chris.r.io/static/css/main.ddd52faa.chunk.css"
+  },
+  {
+    "revision": "c87b7be019e482e6f1cc",
+    "url": "/chris.r.io/static/js/2.da7cc2fc.chunk.js"
+  },
+  {
+    "revision": "0fc61ce0ee2d491f41ee7634c63d9b90",
+    "url": "/chris.r.io/static/js/2.da7cc2fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbf2c7f1e228d0384147",
+    "url": "/chris.r.io/static/js/main.877ae9ff.chunk.js"
+  },
+  {
+    "revision": "3474f7bf7b6ffcd750e8",
+    "url": "/chris.r.io/static/js/runtime-main.1b7407ae.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/chris.r.io/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/chris.r.io/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/chris.r.io/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/chris.r.io/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/chris.r.io/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "aa42a77d0cea977ff1baa6c56124fc39",
+    "url": "/chris.r.io/static/media/logo.aa42a77d.png"
+  },
+  {
+    "revision": "24f7b0944e9e03a905f9d7701573b2cd",
+    "url": "/chris.r.io/static/media/open-sans-latin-300.24f7b094.woff2"
+  },
+  {
+    "revision": "a7622f60c56ddd5301549a786b54e6e6",
+    "url": "/chris.r.io/static/media/open-sans-latin-300.a7622f60.woff"
+  },
+  {
+    "revision": "6725c3fb1f3abd7ebdcb2d244758f706",
+    "url": "/chris.r.io/static/media/open-sans-latin-300italic.6725c3fb.woff2"
+  },
+  {
+    "revision": "793b1237017aeacd646fb80911425566",
+    "url": "/chris.r.io/static/media/open-sans-latin-300italic.793b1237.woff"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "/chris.r.io/static/media/open-sans-latin-400.33543c5c.woff2"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/chris.r.io/static/media/open-sans-latin-400.de0869e3.woff"
+  },
+  {
+    "revision": "06b4bfda4e139eaf3ab9872a6d66f42f",
+    "url": "/chris.r.io/static/media/open-sans-latin-400italic.06b4bfda.woff"
+  },
+  {
+    "revision": "b61a9055d92ac56c15cacf4271d4f072",
+    "url": "/chris.r.io/static/media/open-sans-latin-400italic.b61a9055.woff2"
+  },
+  {
+    "revision": "449d681cd6006390e1bee3c3a660430b",
+    "url": "/chris.r.io/static/media/open-sans-latin-600.449d681c.woff"
+  },
+  {
+    "revision": "819af3d3abdc9f135d49b80a91e2ff4c",
+    "url": "/chris.r.io/static/media/open-sans-latin-600.819af3d3.woff2"
+  },
+  {
+    "revision": "56e5756b696615d6164a625e1bcb1a9e",
+    "url": "/chris.r.io/static/media/open-sans-latin-600italic.56e5756b.woff"
+  },
+  {
+    "revision": "ebdfa24cb411ba3016abd1c3ea5746e5",
+    "url": "/chris.r.io/static/media/open-sans-latin-600italic.ebdfa24c.woff2"
+  },
+  {
+    "revision": "0edb76284a7a0f8db4665b560ee2b48f",
+    "url": "/chris.r.io/static/media/open-sans-latin-700.0edb7628.woff2"
+  },
+  {
+    "revision": "1f85e92d8ff443980bc0f83ad7b23b60",
+    "url": "/chris.r.io/static/media/open-sans-latin-700.1f85e92d.woff"
+  },
+  {
+    "revision": "a49393f2171a2f4d6927ce6c14f73dbc",
+    "url": "/chris.r.io/static/media/open-sans-latin-700italic.a49393f2.woff2"
+  },
+  {
+    "revision": "bf72679ca22e53320beaea090e8bb07d",
+    "url": "/chris.r.io/static/media/open-sans-latin-700italic.bf72679c.woff"
+  },
+  {
+    "revision": "05ebdbe10796850f045fcd484f35788d",
+    "url": "/chris.r.io/static/media/open-sans-latin-800.05ebdbe1.woff"
+  },
+  {
+    "revision": "ab3e500e7375695d702cff19513e4470",
+    "url": "/chris.r.io/static/media/open-sans-latin-800.ab3e500e.woff2"
+  },
+  {
+    "revision": "92da6f116d973bd334cf9b3afdb29c4f",
+    "url": "/chris.r.io/static/media/open-sans-latin-800italic.92da6f11.woff"
+  },
+  {
+    "revision": "c62c3eea77cf3fca51494e8c924adf4d",
+    "url": "/chris.r.io/static/media/open-sans-latin-800italic.c62c3eea.woff2"
+  },
+  {
+    "revision": "6da7725517d8e762d06b28e2b492a352",
+    "url": "/chris.r.io/static/media/portfolioImg.6da77255.jpg"
+  }
+]);
